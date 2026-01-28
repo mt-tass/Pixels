cbe9b7e243277c8f670af52063e1295c106107da
mt-tass <mustafataherramp@gmail.com>
2026-01-29 02:35:51 +0530

    update includes

--- 6_new_projects/include/blocks.hpp
+++ 6_new_projects/include/blocks.hpp
@@ -5,7 +5,31 @@
 #include <vector>
 
 //Live Video Block
-//Light Detection Block
-//Image Overlay Block
+class getVideo{
+    private:
+        cv::VideoCapture capture;
+        bool isOpen;
+    public:
+        getVideo(int cam_idx=0);
+        ~getVideo();
+        cv::Mat getFrame();
+        void release();
+        void isOpened();
 
+};
+//Light Detection Block
+class lightDetector{
+    private:
+        int minVal , minArea , maxArea;
+        cv::Scalar lowerBound , upperBound;
+    public:
+        lightDetector(int min_thresh=245 , int min_area =8500,int max_area= 10000);
+        cv::Point get_bp(const cv::Mat &frame);
+};
+//Image Overlay Block
+class imageOverlay{
+    public:
+        imageOverlay() = default;
+        cv::Mat putImage(const cv::Mat &bg ,const cv::Mat &fg, cv::Point position, double scale=1.0 );
+};
 #endif
