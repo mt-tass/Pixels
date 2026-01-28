8b162d90abec1715974bfbf647213c492314d55f
mt-tass <mustafataherramp@gmail.com>
2026-01-28 22:56:29 +0530

    initial commit

---
+++ 6_new_projects/include/blocks.hpp
@@ -0,0 +1,11 @@
+#ifndef BLOCKS_HEADER
+#define BLOCKS_HEADER
+
+#include <opencv4/opencv2/opencv.hpp>
+#include <vector>
+
+//Live Video Block
+//Light Detection Block
+//Image Overlay Block
+
+#endif
